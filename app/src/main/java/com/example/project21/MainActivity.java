123e9a33c84f7e589f145a16f96e87601f4e75c3
Datentyp21 <luti21lp@gmail.com>
2021-04-23 22:50:50 +0200

    Frontend - update
    MainActivity.java
          - added: Prototype of the ProgressBar
          - tips/notes: Backend not completed yet

--- app/src/main/java/com/example/project21/MainActivity.java
+++ app/src/main/java/com/example/project21/MainActivity.java
@@ -2,15 +2,50 @@ package com.example.project21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bar = findViewById(R.id.progressBar);
+
+
+        this.initialiseBar();
+
+    }
+    private void initialiseBar(){
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                try {
+
+                    while(i < 100) {
+                        Thread.sleep(1000);
+                        i += 10;
+                        bar.setProgress(i);
+                    }
+
+
+                } catch(Exception exc){
+                    exc.printStackTrace();
+                }
+
+            }
+        });
+
+        thread.start();
 
     }
 }
