4452924f90fc0c7d16cdc940fc2b060b1688f0b8
Julian <julian@hage.com>
2021-04-23 21:30:02 +0200

    Frontend - update
    MainActivity.java
          - removed: none
          - added: comment
          - tips/notes: use this information wisely

--- app/src/main/java/com/example/project21/MainActivity.java
+++ app/src/main/java/com/example/project21/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         //irgendeine Scheiße; HIHIHI ; HAHAHAHAHAHAHAHAHAHA
         //tim ist ein Yarro und er kann nichts dagegen machen XD;
         //DAS IST JA ECHT fabuLOUs
+        //Tim how could you leave us? - NF community aka Me, take the T and A out of Team and you are left with ME.
         setContentView(null);
     }
 }
