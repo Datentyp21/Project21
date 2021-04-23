a266542ecc6ae2ddbfb520e97cbea6f1f80f3782
Datentyp21 <luti21lp@gmail.com>
2021-04-23 20:32:41 +0200

    Diese gut

--- app/src/main/java/com/example/project21/MainActivity.java
+++ app/src/main/java/com/example/project21/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //irgendeine Scheiße; HIHIHI
+        //irgendeine Scheiße; HIHIHI ; HAHAHAHAHAHAHAHAHAHA
         setContentView(null);
     }
 }
