7f6d94a18039215e5fefc75f0b2e0df98848e28d
AlexanderHage <alexander@hage.com>
2021-04-23 20:22:32 +0200

    newly modified Shit

--- app/src/main/java/com/example/project21/MainActivity.java
+++ app/src/main/java/com/example/project21/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //irgendeine Scheiße; HIHIHI
+        setContentView(null);
     }
 }
