f12fb5bae8da8429e697e331bae95bd8f394613a
AlexanderHage <alexander@hage.com>
2021-04-30 18:50:19 +0200

    Frontend - update
    MainActivity.java
          - removed: /
          - added: transition between loading screen and login screen
          - tips/notes: (4000 ms delay)

--- app/src/main/java/com/example/project21/MainActivity.java
+++ app/src/main/java/com/example/project21/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.project21;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.project21.ui.login.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
 
     private ProgressBar bar;
 
@@ -16,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
 
         bar = findViewById(R.id.progressBar);
 
