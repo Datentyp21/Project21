44dc565b8ce14460d62e033d000d170663da247f
Datentyp21 <luti21lp@gmail.com>
2021-05-14 22:16:13 +0200

    Frontend - update
    - integrated first version of home screen

--- app/src/main/java/com/example/project21/ui/Login/LoginActivity.java
+++ app/src/main/java/com/example/project21/ui/Login/LoginActivity.java
@@ -1,18 +1,35 @@
 package com.example.project21.ui.Login;
 
-import android.app.Activity;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.project21.R;
+import com.example.project21.ui.Login.fragments.NavigationActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button = findViewById(R.id.login_button);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+                Intent homeIntent = new Intent(LoginActivity.this, NavigationActivity.class);
+                startActivity(homeIntent);
+                finish();
+
     }
 }
