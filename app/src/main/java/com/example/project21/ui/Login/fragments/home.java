105d8c6000cc08fc6b1bc0e1f22aaa59f3fc2225
Julian <julian@hage.com>
2021-05-14 22:03:57 +0200

    Frontend - update
          - changed: Navigationbar

---
+++ app/src/main/java/com/example/project21/ui/Login/fragments/home.java
@@ -0,0 +1,22 @@
+package com.example.project21.ui.Login.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.project21.R;
+
+public class home extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        boolean attachRoot;
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home,container, attachRoot = false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/project21/ui/Login/fragments/notifications.java
@@ -0,0 +1,24 @@
+package com.example.project21.ui.Login.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.project21.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class notifications extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        boolean attachRoot;
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_notifications,container, attachRoot = false);
+        return rootView;
+    }
+}
